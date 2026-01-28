f3071805c446681e15ba31ed701801dcd39b7ac6
DualDustin <dualdustin@gmail.com>
2026-01-28 15:04:13 -0700

    Commit assignment 1 after Question to keep version in git

---
+++ Assignment 1.py
@@ -0,0 +1,36 @@
+### Question 1
+# Make program that mult's consec ints from 1 to product becomes greater then threshold
+# 
+#   store threshold
+#   track current multiplier
+#   print final product
+###
+
+def printMultiplyUptoThreshold(threshold):
+    # track current product, and current Multiplier
+    totalProduct = 1
+    currentNumberMultiplier = 1
+
+    # loop until product is bigger then the threshold
+    while totalProduct <= threshold:
+        # multiply current multiplier with previous product
+        # then add one to the multiplier
+        totalProduct = totalProduct * currentNumberMultiplier
+        currentNumberMultiplier = currentNumberMultiplier + 1
+
+    # subtract multiplier to actual current multiplier
+    currentNumberMultiplier = currentNumberMultiplier - 1
+
+    # show current Multiplier, and end product
+    print("Current Multiplier: " + str(currentNumberMultiplier))
+    print("Product reached: " + str(totalProduct))
+
+
+printMultiplyUptoThreshold(100)
+
+
+###
+# Question 2
+# 
+# 
+###
