41ae7725a9c60bc83fe1b7a02c606fdf86b34d22
DualDustin <dualdustin@gmail.com>
2026-01-29 00:57:48 -0700

    Commit Assignment 1 Question 2 completion to send to git and save to history

--- Assignment 1.py
+++ Assignment 1.py
@@ -36,12 +36,62 @@ printMultiplyUptoThreshold(100)
 #   then the value is a list that lists 
 ###
 
-def dictionaryTheString(stringInput):
+def dictionaryTheStringProperties(stringInput):
+    # initial variable setting up
+    parityOfWord = ""
     lastIndexNumber = 0
     currentIndexNumber = 0
     dictionaryOfWordsInString = {}
 
+    # loop through all characters in String Input
     for characterInStringInput in stringInput:
+        # if characters are seperated by space, its a word
         if characterInStringInput == " ":
-            dictionaryTheString[lastIndexNumber, currentIndexNumber] = "length: " + ""
+            # make the word stored in its own variable
+            wordChosenWithinInput = stringInput[lastIndexNumber:currentIndexNumber]
+
+            # if its not the first word then remove the space at the start
+            if (lastIndexNumber != 0):
+                wordChosenWithinInput = stringInput[lastIndexNumber+1:currentIndexNumber]
+        
+            # determines length and parity of word
+            lengthOfWordWithinInput = len(wordChosenWithinInput)
+            parityOfWord = "odd"
+            if (lengthOfWordWithinInput % 2 == 0):
+                parityOfWord = "even"
+
+            # add the word and its properties into the dictionary
+            # and update last index where a space was found
+            dictionaryOfWordsInString[wordChosenWithinInput] = {"length": lengthOfWordWithinInput, "parity": parityOfWord}
+            lastIndexNumber = currentIndexNumber
+        
+        # update current index
         currentIndexNumber = currentIndexNumber + 1
+
+    # deals with last word in String
+    # get the word without the space
+    # get the length and parity
+    # then add to the dictionary
+    wordChosenWithinInput = stringInput[lastIndexNumber+1:currentIndexNumber]
+    lengthOfWordWithinInput = len(wordChosenWithinInput)
+    parityOfWord = "odd"
+    if (lengthOfWordWithinInput % 2 == 0):
+        parityOfWord = "even"
+
+
+    dictionaryOfWordsInString[wordChosenWithinInput] = {"length": lengthOfWordWithinInput, "parity": parityOfWord}
+
+    return(dictionaryOfWordsInString)
+
+print(dictionaryTheStringProperties("data science rules lol"))
+
+
+
+### Question 3 
+# Make function that that computes x^y
+# given a list of pairs [x,y]
+# 
+#   use for loop to unpack list pair
+#   skip pairs where y is negative
+#   store valid results and prints as list
+###
