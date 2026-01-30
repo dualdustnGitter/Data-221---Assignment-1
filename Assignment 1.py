983fafe9e50fc6868110443981352f226dabc62e
DualDustin <dualdustin@gmail.com>
2026-01-29 20:48:52 -0700

    Commit question 8 completion to send to remote git

--- Assignment 1.py
+++ Assignment 1.py
@@ -291,13 +291,31 @@ def convertSecondsToExpanded(secondsInput):
             + "\n\tPeriod: " + periodElement)
 
 print(convertSecondsToExpanded(45030))
+print()
 
 
 ### Question 8
-# create a function that converts seconds (since midnight) into
+# create dataframe with provided column data
 #   
-#   Hours
-#   Minutes
-#   Seconds 
-#   AM/PM
+#   after converting dictionary to DataFrame
+#   add another column that's derived from existing columns (here ill just make the deriving be the sum)
 ###  
+print("Question 8:")
+
+# import pandas library ans assign it as "pd"
+import pandas as pd
+
+# take in data (dictionary) input 
+data = {
+    "A": [1,2,2,1],
+    "B": [3.1,4.2,1.5,6.3],
+    "C": [800,150,400,210]
+}
+
+# convert dictionary to DataFrame usign pandas
+dataFrameConverted = pd.DataFrame(data)
+
+# derive a new column "D" from summing up values in column "A", "B" and "C"
+# then print it
+dataFrameConverted["D"] = dataFrameConverted["A"] + dataFrameConverted["B"] + dataFrameConverted["C"]
+print(dataFrameConverted)
